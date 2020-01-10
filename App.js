3bf348268dcfbf1259bd7c65d9388daf5c0bf719
Shingo Konnai <shingok@Shingos-iMac.local>
2020-01-09 16:53:39 -0800

    first commit

--- App.js
+++ App.js
@@ -11,6 +11,11 @@ export default function App() {
       <Text>electron-webpack.js plays a key role to give us this magic.</Text>
       <Text>In electron/main/index.js, browserWindow.loadURL method allows us to use hot-reloading.</Text>
       <Text>yarn expo-electron start</Text>
+      <Text>References:</Text>
+      <Text>- Blog: https://dev.to/evanbacon/making-desktop-apps-with-electron-react-native-and-expo-5e36</Text>
+      <Text>- Example repo: https://github.com/expo/examples/tree/master/with-electron</Text>
+      <Text>- Docs: https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/guides/using-electron.md#building-your-project</Text>
+      <Text>- Others: https://www.npmjs.com/package/@expo/electron-adapter</Text>
     </View>
   );
 }
